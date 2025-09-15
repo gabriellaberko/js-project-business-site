b51fb3148365fc5660c0838f33a64e3d3768b70c
”gabriellaberko” <75434964+gabriellaberko@users.noreply.github.com>
2025-09-15 11:17:20 +0200

    add hamburger menu on small screens

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Include a responsive hamburger menu that's visible on smaller screens but hidden on bigger screens.
+When you click, all links in your navbar become visible, and when you click again, they are hidden
+
+- What will your menu items look like?
+- What should happen when you click the hamburger icon?
+- How will you show and hide the menu?
+*/
+
+
+/* TO-DO: create variable for hamburger icon */ 
+const hamburgerIcon = document.getElementById("icon")
+
+/* TO-DO: use onclick event for hamburger icon - nav links should expand and have a background color set */ 
