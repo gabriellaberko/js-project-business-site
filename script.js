4f81e178e7784988f2e80d78acbb473986b47d59
”gabriellaberko” <75434964+gabriellaberko@users.noreply.github.com>
2025-09-15 17:20:41 +0200

    add minor styling for navbar and clean up code

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-/*
-Include a responsive hamburger menu that's visible on smaller screens but hidden on bigger screens.
-When you click, all links in your navbar become visible, and when you click again, they are hidden
-
-- What will your menu items look like?
-- What should happen when you click the hamburger icon?
-- How will you show and hide the menu?
-*/
-
-
-/* create variable for hamburger menu icon and nav links */ 
 const hamburgerIcon = document.getElementById("hamburgerIcon")
 const crossIcon = document.getElementById("crossIcon")
 const navBar = document.getElementById("nav")
 const menuIcon = document.getElementById("menuIcon")
 const navLinks = document.getElementById("links");
 
-
-
 function expandCollapseHamburgerMenu() {
   if (navLinks.style.display === "block") {
     navLinks.style.display = "none";
+    navBar.style.backgroundColor = "transparent";
   } else {
     navLinks.style.display = "block";
+    navBar.style.backgroundColor = "#eeeeee21"; 
   }
 }
 
